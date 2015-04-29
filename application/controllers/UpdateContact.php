e7509a222facd55088295c6d2e22b827c1b567e1
isharegalaxy <ruanzhe114@163.com>
2015-04-29 14:33:24 +0800

    优化代码

--- application/controllers/UpdateContact.php
+++ application/controllers/UpdateContact.php
@@ -18,32 +18,34 @@ class UpdateContact extends CI_Controller
         $this->load->database();
         //TESTURL = http://localhost/ishare_server/index.php/updatecontact?phone=18500138088&key=123456&data={%22del%22:[{%22phone%22:13810459534},{%22phone%22:13810459533}],%22add%22:[{%22phone%22:18500138088}]}
 
-        if(array_key_exists('data', $_POST)) {
-            $dataStr = $_POST['data'];
+        if(array_key_exists('add', $_POST)) {
+            $addStr = $_POST['add'];
 
-            $data = json_decode($dataStr);
-            if(property_exists($data, 'add')) {
-                foreach($data->add as $val) {
-                    $contact = $val->phone;
-                    if(!$contact) {
-                        continue;
-                    }
-                    $query = $this->db->query("select * from contacts where host_phone='$phone' and contact_phone='$contact'");
-                    if($query->num_rows() === 0) {
-                        $this->db->query("insert into contacts (host_phone, contact_phone) values('$phone', '$contact')");
-                    }
+            $addList = json_decode($addStr);
+            foreach ($addList as $val) {
+                $contact = $val->phone;
+                if (!$contact) {
+                    continue;
+                }
+                $query = $this->db->query("select * from contacts where host_phone='$phone' and contact_phone='$contact'");
+                if ($query->num_rows() === 0) {
+                    $this->db->query("insert into contacts (host_phone, contact_phone) values('$phone', '$contact')");
                 }
             }
+        }
 
-            if(property_exists($data, 'del')) {
-                foreach($data->del as $val) {
-                    $contact = $val->phone;
-                    if(!$contact) {
-                        continue;
-                    }
-                    $this->db->query("delete from contacts where host_phone='$phone' and contact_phone='$contact'");
+        if(array_key_exists('del', $_POST)) {
+            $delStr = $_POST['del'];
+
+            $delList = json_decode($delStr);
+            foreach ($delList as $val) {
+                $contact = $val->phone;
+                if (!$contact) {
+                    continue;
                 }
+                $this->db->query("delete from contacts where host_phone='$phone' and contact_phone='$contact'");
             }
+
         }
 
         $this->db->close();
